cbad8fbd58865441109fefc6c8e0e272ae675419
Melvin <59696649+Klackson@users.noreply.github.com>
2023-12-08 00:04:50 +0100

    Memory implementation
    - Memory creation
    - Re-tooled treesearch so that it works on beliefstates rather than results

--- src/logic/AI.java
+++ src/logic/AI.java
@@ -105,37 +105,40 @@ class Result{
  */
 public class AI{
 	
-	public HashMap<Result,Float> memory; 
+	// We want our memory to work on the most general case as possible, and since results can be much more diverse than beliefstates, we use belifstates as keys to our memory
+	public static HashMap<String,Float> memory = new HashMap<String, Float>(); 
 	
 	// Parameters to fiddle with
-	public static int maxdepth = 5;
+	public static int maxdepth = 6;
 	public static String aggregate_method = "mean";
 	public static int value_per_life = 15;
-	public static int death_penatly = 10;
+	public static int death_penatly = 1000;
 	
-	public static int heuristic (Result result) {
+	
+	public static int heuristic (BeliefState bstate) {
 		
-		// Very  primitive heuristic, we only consider current score
+		// Very  primitive heuristic, we only consider current score and wether pacman is alive or dead
 		int hscore = 0;
-		for(BeliefState bstate : result.getBeliefStates()) {
-			hscore+= bstate.getScore();
-			//hscore+= bstate.getLife() * value_per_life; No point in that as long as we only have a single life
-			if (bstate.getLife()==0) hscore -= death_penatly;
-		}
+		hscore+= bstate.getScore();
+		//hscore+= bstate.getLife() * value_per_life; No point in that as long as we only have a single life
+		if (bstate.getLife()==0) hscore -= death_penatly;
 
 		return hscore;
 	}
 	
+	
+	// This method will define what function we use in order to compute the values from several children (originally leaves)
 	public static float aggregateValues(ArrayList<Float> values) {
 		
 		// Mean version
 		if (aggregate_method=="mean") {
-			float sum=0;
-			for(float value : values) {
+			float sum= 0;
+			for(Float value : values) {
 				sum+=value;
 			}
 			return sum/values.size();
 		}
+		
 		else if (aggregate_method=="min"){
 			float min = Integer.MAX_VALUE;
 			for (float value : values){
@@ -146,22 +149,34 @@ public class AI{
 		return 0;
 	}
 	
-	public static float treesearch(Result result, int currentdepth) {
-		//should this work with results or belief states ?
+	
+	// As we save values based on beliefstates and not on results, had to rework this function to work on  beliefstates
+	public static float treesearch(BeliefState bstate, int currentdepth) {
 		
-		if(currentdepth == maxdepth) return heuristic(result);
+		// If we reach a leaf, stop expanding and return the heuristic value
+		if(currentdepth == maxdepth) return heuristic(bstate);
 		
+		// If this beliefstate has already been considered, output the already known answer
+		if(memory.containsKey(bstate.toString())) {
+			return memory.get(bstate.toString());
+		}
+		
+		// Get every result achievable from this beliefstate
+		ArrayList<Result> results = bstate.extendsBeliefState().results;
+		
+		float child_value;
 		ArrayList<Float> children_values = new ArrayList<Float>();
 		
-		for(BeliefState bstate : result.getBeliefStates()) {
-			
-			ArrayList<Result> results = bstate.extendsBeliefState().results;
-			for(Result child : results){
-				children_values.add( treesearch(child, currentdepth+1) );
+		// We are gonna throw a recursive call on each beliefstate of each one of the possible results
+		for (Result child : results) {
+			for (BeliefState beliefchild : child.getBeliefStates()) {
+				
+				child_value = treesearch(beliefchild, currentdepth+1);
+				memory.put(beliefchild.toString(), child_value); // Obviously, save that result in the memory
+				children_values.add(child_value);
 			}
-			
 		}
-
+		
 		return aggregateValues(children_values);
 	}
 	
@@ -172,21 +187,29 @@ public class AI{
 	 * @return a string describing the next action (among PacManLauncher.UP/DOWN/LEFT/RIGHT)
 	 */
 	public static String findNextMove(BeliefState beliefState) {
-
+		//memory = new HashMap<String, Float>(); Tried reseting memory at every new search to reduce containsKey runtime : not a good idea
 		Plans plans = beliefState.extendsBeliefState();
-
+		
 		float max_utility = Integer.MIN_VALUE;
 		float plan_utility;
 		String chosen_action = PacManLauncher.LEFT;
-
+		ArrayList<Float> belief_utilities;
+		
+		// We are going to expand all possible beliefstates possible after each action and select the action with the best aggregate utility
 		for(int i=0; i<plans.size(); i++){
-			plan_utility = treesearch(plans.getResult(i), 0);
+			Result plan_result = plans.getResult(i);
+			String plan_action = plans.getAction(i).get(0);
+			belief_utilities = new ArrayList<Float>();
+			
+			for(BeliefState bstate : plan_result.getBeliefStates()) {
+				belief_utilities.add(treesearch(bstate, 0));
+			}			
+			plan_utility = aggregateValues(belief_utilities);
 			if(plan_utility > max_utility){
 				max_utility = plan_utility;
-				chosen_action = plans.getAction(i).get(0);
+				chosen_action = plan_action;
 			}
 		}
-
 		return chosen_action;
 	}
 }
