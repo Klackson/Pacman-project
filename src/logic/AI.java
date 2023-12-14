a86d99a1e0299efce63709fca43ed8b5ea246fe6
Melvin <melvingode123@gmail.com>
2023-12-14 16:44:10 +0100

    changed Treesearch to do max-mean instead of mean-mean
    Also changed the time when pacman uses memory checks

--- src/logic/AI.java
+++ src/logic/AI.java
@@ -104,21 +104,23 @@ class Result{
  * class implement the AI to choose the next move of the Pacman
  */
 public class AI{
+
+	public static String[] pacmanactions = new String[]{"UP","LEFT","RIGHT","DOWN"};
 	
 	// We want our memory to work on the most general case as possible, and since results can be much more diverse than beliefstates, we use belifstates as keys to our memory
 	public static HashMap<String,Float> memory = new HashMap<String, Float>(); 
 	
 	// Parameters to fiddle with
-	public static int maxdepth = 6;
+	public static int maxdepth = 4;
 	public static String aggregate_method = "mean";
 	public static int value_per_life = 15;
-	public static int death_penatly = 1000;
+	public static int death_penatly = 10_00;
 	
 	
-	public static int heuristic (BeliefState bstate) {
+	public static float heuristic (BeliefState bstate) {
 		
 		// Very  primitive heuristic, we only consider current score and wether pacman is alive or dead
-		int hscore = 0;
+		float hscore = 0;
 		hscore+= bstate.getScore();
 		//hscore+= bstate.getLife() * value_per_life; No point in that as long as we only have a single life
 		if (bstate.getLife()==0) hscore -= death_penatly;
@@ -129,14 +131,16 @@ public class AI{
 	
 	// This method will define what function we use in order to compute the values from several children (originally leaves)
 	public static float aggregateValues(ArrayList<Float> values) {
-		
+		if(values.isEmpty())return 0;
 		// Mean version
 		if (aggregate_method=="mean") {
 			float sum= 0;
 			for(Float value : values) {
 				sum+=value;
 			}
-			return sum/values.size();
+			float v = sum/values.size();
+			return v;
+			
 		}
 		
 		else if (aggregate_method=="min"){
@@ -156,28 +160,31 @@ public class AI{
 		// If we reach a leaf, stop expanding and return the heuristic value
 		if(currentdepth == maxdepth) return heuristic(bstate);
 		
-		// If this beliefstate has already been considered, output the already known answer
-		if(memory.containsKey(bstate.toString())) {
-			return memory.get(bstate.toString());
-		}
-		
-		// Get every result achievable from this beliefstate
-		ArrayList<Result> results = bstate.extendsBeliefState().results;
-		
 		float child_value;
-		ArrayList<Float> children_values = new ArrayList<Float>();
-		
-		// We are gonna throw a recursive call on each beliefstate of each one of the possible results
-		for (Result child : results) {
-			for (BeliefState beliefchild : child.getBeliefStates()) {
-				
-				child_value = treesearch(beliefchild, currentdepth+1);
-				memory.put(beliefchild.toString(), child_value); // Obviously, save that result in the memory
+		float max_actionvalue = Float.MIN_VALUE;
+
+
+		for(String action : pacmanactions){
+
+			ArrayList<Float> children_values = new ArrayList<Float>();
+
+			Result result = bstate.extendsBeliefState(action);
+			for(BeliefState beliefchild : result.getBeliefStates()){
+				if(memory.containsKey(beliefchild.toString())) {
+					// If this beliefstate has already been considered, output the already known answer
+					child_value =  memory.get(beliefchild.toString());
+				}
+				else {
+					child_value = treesearch(beliefchild, currentdepth + 1);
+					memory.put(beliefchild.toString(), child_value); // Obviously, save that result in the memory
+				}
 				children_values.add(child_value);
 			}
+			float actionvalue = aggregateValues(children_values);
+			if(actionvalue > max_actionvalue) max_actionvalue = actionvalue;
 		}
-		
-		return aggregateValues(children_values);
+
+		return max_actionvalue;
 	}
 	
 	/**
@@ -205,6 +212,7 @@ public class AI{
 				belief_utilities.add(treesearch(bstate, 0));
 			}			
 			plan_utility = aggregateValues(belief_utilities);
+			
 			if(plan_utility > max_utility){
 				max_utility = plan_utility;
 				chosen_action = plan_action;
