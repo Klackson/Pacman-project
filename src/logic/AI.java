38d8b5903b9b1baa41a0222be56e102e7f2b9cb8
Melvin <59696649+Klackson@users.noreply.github.com>
2023-12-18 16:35:27 +0100

    - Fiddled a little with nearest_gom_distance to improve incentives
    Under the right parameters this version seems to reach level 2 with decent consistency and scored above 4000.
    Still performing much much better when being chased by ghosts though, try exploring this idea.
    Average compute time : ~60ms

--- src/logic/AI.java
+++ src/logic/AI.java
@@ -103,34 +103,32 @@ class Result{
  */
 public class AI{
 
-	public static String[] pacmanactions = new String[]{"UP","LEFT","RIGHT","DOWN"};
-	
 	// We want our memory to work on the most general case as possible, and since results can be much more diverse than beliefstates, we use belifstates as keys to our memory
 	public static TreeMap<String,Float> memory = new TreeMap<String, Float>();
 	
 	// Parameters to fiddle with
 
-	static int memory_refresh_rate = 3;
-	static int maxdepth = 3;
-	static String aggregate_method = "min";
-	static int value_per_life = 15;
-	static int death_penatly = 1000;
+	static final int memory_refresh_rate = 3; // This value HAS to be below maxdepth otherwise pacman literally won't see ghosts coming
+	static final int maxdepth = 4;
+	static final String aggregate_method = "mean";
+	static final float death_penatly = 1000;
 
-	static final int turnback_penalty = 98;
+	static final float turnback_penalty = 98;
+	// Intentionally right under a hundred to make it still worthwile turning back to eat a ghost
+	static final float max_expand = 5;
+
 
 	static int number_of_moves = 0;
 
-	static final float max_expand=10;
-	
-	
 	public static float heuristic (BeliefState bstate) {
-		if (bstate.getLife()==0) return 0;
+		//if (bstate.getLife()==0) return 0;
 		// Very  primitive heuristic, we only consider current score and wether pacman is alive or dead
 		float hscore = 0;
 		hscore+= bstate.getScore();
+		hscore-= (float) bstate.getNbrOfGommes();
 		//hscore+= bstate.getLife() * value_per_life; No point in that as long as we only have a single life
 		if (bstate.getLife()==0) hscore -= death_penatly;
-		hscore -= 2 * nearest_gom_distance(bstate);
+		hscore -= 10 * nearest_gom_distance(bstate);
 
 		return hscore;
 	}
@@ -145,8 +143,7 @@ public class AI{
 			for(Float value : values) {
 				sum+=value;
 			}
-			float v = sum/values.size();
-			return v;
+            return sum/values.size();
 		}
 		
 		else if (Objects.equals(aggregate_method, "min")){
@@ -155,6 +152,8 @@ public class AI{
 		return 0;
 	}
 
+	// Checks if two directions are opposite
+	// Is used as a local heuristic to penalize U-turns
 	public static boolean opposite_direction(String direction1, char direction2){
         return (Objects.equals(direction1, "UP") && direction2== 'D') ||
                 (Objects.equals(direction1, "DOWN") && direction2 == 'U') ||
@@ -162,45 +161,65 @@ public class AI{
                 (Objects.equals(direction1, "LEFT") && direction2 == 'R');
     }
 
-	public static int nearest_gom_distance(BeliefState bstate){
-		Position pacmanpos = bstate.getPacmanPos();
+	public static boolean same_direction(String direction1, char direction2){
+		return (Objects.equals(direction1, "UP") && direction2== 'U') ||
+				(Objects.equals(direction1, "DOWN") && direction2 == 'D') ||
+				(Objects.equals(direction1, "RIGHT") && direction2 == 'R') ||
+				(Objects.equals(direction1, "LEFT") && direction2 == 'L');
+	}
 
-		int max_search_distance = 25;
+	// Checks for the distance to the nearest existing gom
+	// Is used in the heuristic to incentivize going towards goms
+	public static int nearest_gom_distance(BeliefState bstate){
+		if(bstate.getNbrOfGommes()==0) return -10000; // -10000 is actually a big bonus (since we substract this result in the heuristic) and we give if the map is completed
+
+		Position pacmanpos = bstate.getPacmanPosition();
+		int pacmanx = pacmanpos.x; int pacmany = pacmanpos.y;
+
+		char[][] map = bstate.getMap();
+
+		int max_search_distance = 15;
 		int i; int k; int j;
 		for(k=1; k<= max_search_distance; k++){
 			for(i=-k; i<=k; i++){
 				for(j=-k; j<=k; j++){
-					if(pacmanpos.x + i >= 25 ||
-					pacmanpos.x +i < 0 ||
-					pacmanpos.y + j >= 25 ||
-					pacmanpos.y +j < 0)continue;
-					if(bstate.getMap(pacmanpos.x+i, pacmanpos.y+j)=='.') {
+					if(pacmanx + i >= 25 ||
+					pacmanx + i < 0 ||
+					pacmany + j >= 25 ||
+					pacmany +j < 0)continue;
+					if( map[pacmanx + i][pacmany + j] =='.') {
+						if(k<=1) return -20;
 						return Math.abs(i) + Math.abs(j);
 					}
 				}
 			}
 		}
-		return 70;
+		System.out.println("gom not found. Move "+ number_of_moves);
+		return 2 * max_search_distance;
 	}
 	
 	
 	// As we save values based on beliefstates and not on results, had to rework this function to work on  beliefstates
 	public static float treesearch(BeliefState bstate, int currentdepth) {
-		float childvalue;
+
+		// If we reach a leaf, stop expanding and return the heuristic value
+		if(currentdepth == maxdepth) return heuristic(bstate);
+
 		ArrayList<BeliefState> beliefchildren;
 		ArrayList<Float> children_values;
 		float nbchildren;
 		float expand_proba;
 		Result result;
-		
-		// If we reach a leaf, stop expanding and return the heuristic value
-		if(currentdepth == maxdepth) return heuristic(bstate);
-		
+
 		float child_value;
 		float max_actionvalue = Float.MIN_VALUE;
 
 		Plans plans = bstate.extendsBeliefState();
+
 		for(int i=0; i<plans.size(); i++){
+
+			if (opposite_direction(plans.getAction(i).get(0), bstate.getPacmanPos().getDirection()) && plans.getAction(i).size()==1) continue;
+
 			result = plans.getResult(i);
 			beliefchildren = result.getBeliefStates();
 
@@ -210,6 +229,9 @@ public class AI{
 			children_values = new ArrayList<Float>();
 
 			for(BeliefState beliefchild : beliefchildren){
+				// To ease the computation burden, we don't compute all of the expanded beliefstates
+				// We randomly skip some of them depending on how many there are.
+				// The expected number of children expanded is close to the minimum between the number of children and the parameter max_expand.
 				if(Math.random()> expand_proba)continue;
 
 				if(memory.containsKey(beliefchild.toString())) {
@@ -245,7 +267,7 @@ public class AI{
 	 * @return a string describing the next action (among PacManLauncher.UP/DOWN/LEFT/RIGHT)
 	 */
 	public static String findNextMove(BeliefState beliefState) {
-		if(number_of_moves%memory_refresh_rate==0) memory = new TreeMap<String, Float>(); //Tried reseting memory at every new search to reduce containsKey runtime : not a good idea
+		if(number_of_moves % memory_refresh_rate==0) memory = new TreeMap<String, Float>();
 		Plans plans = beliefState.extendsBeliefState();
 		
 		float max_utility = Integer.MIN_VALUE;
