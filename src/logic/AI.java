c02b3f633f808b38d6fb878ab49bc8d983054793
Melvin <59696649+Klackson@users.noreply.github.com>
2024-01-05 09:18:27 +0100

    ParallelTreeSearch class unification

--- src/logic/AI.java
+++ src/logic/AI.java
@@ -3,8 +3,8 @@ package logic;
 import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
-
 import view.Gomme;
+
 
 
 /**
@@ -110,7 +110,7 @@ public class AI{
 	
 	// Parameters to fiddle with
 
-	static final int memory_refresh_rate = 1;
+	static final int memory_refresh_rate = 100;
 	static final int maxdepth = 4;
 	static final String aggregate_method = "mean";
 	static final float death_penatly = 4000;
@@ -199,7 +199,7 @@ public class AI{
 	}
 
 	public static float heuristic (BeliefState bstate, BeliefState original_bstate) {
-		// if (bstate.getLife()==0) return 0; //Float.MIN_VALUE;
+		if (bstate.getLife()==0) return 0; //Float.MIN_VALUE;
 
 		float hscore = 0;
 
@@ -361,4 +361,77 @@ public class AI{
 	public static synchronized float get_memory_value(TreeMap<BeliefState, Float> memory, BeliefState bstate){
 		return memory.get(bstate);
 	}
+}
+
+class ParallelTreeSearch  extends RecursiveAction {
+	BeliefState bstate;
+	Result result;
+	int currentdepth;
+	BeliefState originalstate;
+	float hvalue = Float.MIN_VALUE;
+	String nodetype;
+	TreeMap<BeliefState, Float> memory;
+
+
+	ParallelTreeSearch (BeliefState bstate, int currentdepth, BeliefState originalstate, TreeMap<BeliefState, Float> memory)
+	{
+		this.bstate = bstate;
+		this.currentdepth = currentdepth;
+		this.originalstate = originalstate;
+		this.memory = memory;
+		nodetype="OR";
+	}
+
+	ParallelTreeSearch (Result result, int currentdepth, BeliefState originalstate, TreeMap<BeliefState, Float> memory)
+	{
+		this.result = result;
+		this.currentdepth = currentdepth;
+		this.originalstate = originalstate;
+		this.memory = memory;
+		nodetype="AND";
+	}
+
+	protected void compute (){
+		if(Objects.equals(nodetype, "OR")) {
+			if (currentdepth == AI.maxdepth || bstate.getLife() == 0) hvalue = AI.heuristic(bstate, originalstate);
+
+			else if (AI.state_in_memory(memory, bstate)) hvalue = AI.get_memory_value(memory, bstate);
+
+			else {
+				ArrayList<Result> results = bstate.extendsBeliefState().results;
+				ArrayList<ParallelTreeSearch> result_children = new ArrayList<>();
+
+				for (Result result : results) {
+					result_children.add(new ParallelTreeSearch(result, currentdepth, originalstate, memory));
+				}
+
+				invokeAll(result_children); // Recursive parallel call
+
+				ArrayList<Float> children_values = new ArrayList<>();
+				for (ParallelTreeSearch child : result_children) {
+					children_values.add(child.hvalue);
+				}
+				hvalue = Collections.max(children_values);
+			}
+			AI.save_result(memory, bstate, hvalue);
+		}
+
+		else{
+			ArrayList<BeliefState> states = result.getBeliefStates();
+			ArrayList<ParallelTreeSearch> belief_children = new ArrayList<>();
+
+			for(BeliefState bstate : states){
+				belief_children.add(new ParallelTreeSearch(bstate, currentdepth+1, originalstate, memory));
+			}
+
+			invokeAll(belief_children);
+
+			ArrayList<Float> children_values = new ArrayList<>();
+			for (ParallelTreeSearch child : belief_children) {
+				children_values.add(child.hvalue);
+			}
+			hvalue = AI.aggregateValues(children_values);
+		}
+	}
+
 }
